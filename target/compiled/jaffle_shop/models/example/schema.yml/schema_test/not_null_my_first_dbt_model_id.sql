b10b66a1e7d9dc8cb4626455c5f73482739b89f5
david.yang@atos.net <david.yang@atos.net>
2021-11-16 05:05:20 +0000

    updated tests, docs, and sources

---
+++ target/compiled/jaffle_shop/models/example/schema.yml/schema_test/not_null_my_first_dbt_model_id.sql
@@ -0,0 +1,9 @@
+
+    
+    
+
+select *
+from `striking-gadget-328313`.`dbt_dyisar`.`my_first_dbt_model`
+where id is null
+
+
---
+++ target/compiled/jaffle_shop/models/staging/jaffle_shop/stg_jaffle_shop.yml/schema_test/not_null_stg_orders_order_id.sql
@@ -0,0 +1,9 @@
+
+    
+    
+
+select *
+from `striking-gadget-328313`.`dbt_dyisar`.`stg_orders`
+where order_id is null
+
+
